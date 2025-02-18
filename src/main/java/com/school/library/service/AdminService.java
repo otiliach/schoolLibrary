da0fc16ed17bccc56363eb593e4d10533362c6f1
otiliach <eotilia1@gmail.com>
2025-02-19 00:04:18 +0200

    I added service and autowired annotations.

--- src/main/java/com/school/library/service/AdminService.java
+++ src/main/java/com/school/library/service/AdminService.java
@@ -4,33 +4,39 @@ import com.school.library.model.Admin;
 import com.school.library.model.RegisterDto;
 import com.school.library.repository.AdminRepository;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
 
+@Service
 public class AdminService implements UserDetailsService {
-
     private final AdminRepository adminRepository;
 
+    @Autowired
     public AdminService(AdminRepository adminRepository) {
         this.adminRepository = adminRepository;
     }
-
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
         Admin adminUser = adminRepository.findByEmail(email);
 
         if(adminUser != null){
+            System.out.println("User found: " + adminUser.getEmail());
             return User.withUsername(adminUser.getEmail())
                     .password(adminUser.getPassword())
                     .roles(adminUser.getRole())
                     .build();
+        }
+        else{
+            System.out.println("User not found");
         }
 
         return null;
