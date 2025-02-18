a6a628b6691a4252bbc2cfa72933886b98ad5ed9
otiliach <eotilia1@gmail.com>
2025-02-19 00:23:08 +0200

    Added view controllers.

--- src/main/java/com/school/library/controller/ViewController.java
+++ src/main/java/com/school/library/controller/ViewController.java
@@ -1,13 +1,33 @@
 package com.school.library.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
-    @GetMapping("/admin/home")
-    public String handleAdminHome(){
-        return "home_admin";
+
+    @GetMapping("/")
+    public String redirect(){
+        return "redirect:/biblioteca";
+    }
+
+    @GetMapping("/biblioteca")
+    public String index(Model model) {
+        model.addAttribute("message", "Biblioteca online");
+        return  "index";
+    }
+
+    @GetMapping("/biblioteca/carte/editare/{id}")
+    public String updateBookForm(Model model) {
+        model.addAttribute("message", "Editează detaliile cărții:");
+        return "update_book";
+    }
+
+    @GetMapping("/biblioteca/carte/adaugare")
+    public String addBookForm(Model model) {
+        model.addAttribute("message", "Introdu detaliile cărții:");
+        return "new_book";
     }
 }
 
