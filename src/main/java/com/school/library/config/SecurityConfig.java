5fe507af6db81be31f75d940432b85a42bf170de
otiliach <eotilia1@gmail.com>
2025-02-19 00:16:34 +0200

    Added SecurityConfig class implementation.

--- src/main/java/com/school/library/config/SecurityConfig.java
+++ src/main/java/com/school/library/config/SecurityConfig.java
@@ -1,9 +1,35 @@
 package com.school.library.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/","/biblioteca").permitAll()
+                        .requestMatchers("/library/all-domains","/library/all-books","/library/books-filtered").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/register").permitAll()
+                        .requestMatchers("/logout").hasRole("admin")
+                        .requestMatchers("/**").hasRole("admin")
+                        .anyRequest().authenticated()
+                ).formLogin(form -> form.defaultSuccessUrl("/", true))
+                .logout(config -> config.logoutSuccessUrl("/"))
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
